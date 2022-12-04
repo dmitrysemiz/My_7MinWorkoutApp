9a925bd3ce2faada5597806d55de8d41ae74fd5a
dmitrysemiz <111497807+dmitrysemiz@users.noreply.github.com>
2022-12-04 19:48:36 +0300

    exerciseTimer on progress bar, visibility

--- app/src/main/java/com/example/my_7minworkoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/example/my_7minworkoutapp/ExerciseActivity.kt
@@ -3,7 +3,7 @@ package com.example.my_7minworkoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.widget.Toast
+import android.view.View
 import com.example.my_7minworkoutapp.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
@@ -12,6 +12,11 @@ class ExerciseActivity : AppCompatActivity() {
     private var restTimer: CountDownTimer? = null
     private var restProgress: Int = 0
     private var restTime: Int = 10 //must match value android:max="" in xml
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress: Int = 0
+    private var exerciseTime: Int = 30 //must match value android:max="" in xml
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +32,29 @@ class ExerciseActivity : AppCompatActivity() {
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         }
         //Enabling toolbar on top, and a back arrow-button
+        setRestView()
+    }
 
+    private fun setRestView(){
+        binding?.flRestView?.visibility = View.VISIBLE
+        binding?.flExerciseView?.visibility = View.GONE
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
         setRestProgressBar()
     }
+
+    private fun setExerciseView(){
+        binding?.flRestView?.visibility = View.GONE
+        binding?.flExerciseView?.visibility = View.VISIBLE
+        if(exerciseTimer != null){
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+        setExerciseProgressBar()
+    }
+
 
     private fun setRestProgressBar(){
         binding?.progressBar?.progress = restProgress
@@ -42,7 +67,23 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(applicationContext, "Start Exercise", Toast.LENGTH_SHORT).show()
+                setExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setExerciseProgressBar(){
+        binding?.progressBarExercise?.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(exerciseTime.toLong()*1000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = exerciseTime - exerciseProgress
+                binding?.tvTimerExercise?.text = (exerciseTime - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                setRestView()
             }
         }.start()
     }
@@ -52,6 +93,10 @@ class ExerciseActivity : AppCompatActivity() {
         if(restTimer != null){
             restTimer!!.cancel()
             restProgress = 0
+        }
+        if(exerciseTimer != null){
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
         }
         binding = null
     }
